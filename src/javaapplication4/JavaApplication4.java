2411bf0c03531efa2e91720b33c168b8426fffab
ntherien9606 <ntherien9606@007LAB163000634.neisd.ad>
2015-11-16 14:41:37 -0600

    Hi friend :) this is my code. it may not make any sense cause i have no clue what I am doing :)

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/*
+ *
+ * @author ntherien9606
+ */
+import java.util.Scanner;
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+      public void solve(int n, String start, String auxiliary, String end) {
+       if (n == 1) {
+           System.out.println(start + " -> " + end);
+       } else {
+           solve(n - 1, start, end, auxiliary);
+           System.out.println(start + " -> " + end);
+           solve(n - 1, auxiliary, start, end);
+       }
+   }
+
+   public static void main(String[] args) {
+       JavaApplication4 towersOfHanoi = new JavaApplication4();
+       System.out.print("Enter number of discs: ");
+       Scanner scanner = new Scanner(System.in);
+       int discs = scanner.nextInt();
+       towersOfHanoi.solve(discs, "A", "B", "C");
+   }
+
+    }
